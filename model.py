a610d9a2005939d739b1a4fa468d1ba433430ba9
CoffeeddCat <zerozmi@163.com>
2018-12-03 10:36:30 +0800

    update.

--- model.py
+++ model.py
@@ -4,9 +4,10 @@ import numpy as np
 
 class Network:
 
-    def __init__(self, config):
+    def __init__(self, config, scope):
         self.config = config
         self.train_step = 0
+        self.scope = scope
         conf = tf.ConfigProto(allow_soft_placement=True,
                               log_device_placement=False)
         conf.gpu_options.allow_growth = True
@@ -21,10 +22,10 @@ class Network:
 
     def initialize_network(self):
         self.image_input = tf.placeholder(
-            tf.float32, shape=[None] + self.config.input_shape, name="image_input")
+            tf.float32, shape=[None] + self.config.input_shape, name="image_input" + self.scope)
         out = self.image_input
         print(out)
-        with tf.variable_scope("cnn_part"):
+        with tf.variable_scope("cnn_part" + self.scope):
             for filters, kernel_size, strides in zip(self.config.filters, self.config.kernel_size, self.config.strides):
                 layer = tf.layers.conv3d(
                     inputs=out,
@@ -40,7 +41,7 @@ class Network:
         self.cnn_output = tf.layers.flatten(out)
         out = self.cnn_output
 
-        with tf.variable_scope("dnn_part"):
+        with tf.variable_scope("dnn_part" + self.scope):
             for output_num in self.config.dnn_shape:
                 layer = tf.layers.dense(
                     inputs=out,
@@ -53,23 +54,23 @@ class Network:
         print(self.dnn_output)
 
         self.standard_mat = tf.placeholder(
-            tf.float32, shape=[None, 16 * 7], name="standard_mat")
-        with tf.variable_scope("train_part"):
+            tf.float32, shape=[None, 16 * 7], name="standard_mat" + self.scope)
+        with tf.variable_scope("train_part" + self.scope):
             self.loss = tf.reduce_mean(
                 (tf.square(self.dnn_output - self.standard_mat)))
             self.trainer = tf.train.AdamOptimizer(
                 self.config.learning_rate).minimize(self.loss)
 
-    def train(self, input_upside_buffer, input_downside_buffer, output_upside_buffer, output_downside_buffer):
+    def train(self, input_buffer, output_buffer):
         self.train_step = self.train_step + 1
         # print(type(input_upside_buffer))
         # print(type(output_upside_buffer))
         _, loss = self.sess.run([self.trainer, self.loss], feed_dict={
-            self.image_input: input_upside_buffer,
-            self.standard_mat: output_upside_buffer
+            self.image_input: input_buffer,
+            self.standard_mat: output_buffer
         })
         if self.train_step % 10 == 1:
-            print("now learning step: %d, now loss: %f" %
+            print(self.scope + "now learning step: %d, now loss: %f" %
                   (self.train_step, loss))
 
         if self.train_step % self.config.every_steps_save == 1:
@@ -86,8 +87,8 @@ class Network:
     def model_save(self, name=None):
         print("now training step %d...model saving..." % (self.train_step))
         if name == None:
-            self.saver.save(self.sess, "model/training_step",
-                       global_step=self.train_step)
+            self.saver.save(self.sess, "model/training_step" + self.scope,
+                            global_step=self.train_step)
         else:
             self.saver.save(self.sess, name)
 
